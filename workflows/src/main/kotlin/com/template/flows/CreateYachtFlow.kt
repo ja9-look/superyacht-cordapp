784f4dcd31ed027eec1efc33645a5a33f5c94201
ja9-look <janineluk95@gmail.com>
2022-10-13 14:34:34 +0100

    Write tests for  createYachtFlow, add flows for createYachtFlow and purchaseYachtFlow, and import finance library.

---
+++ workflows/src/main/kotlin/com/template/flows/CreateYachtFlow.kt
@@ -0,0 +1,76 @@
+package com.template.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.*
+import net.corda.core.identity.*
+import net.corda.core.utilities.ProgressTracker
+import net.corda.core.flows.FinalityFlow
+
+import net.corda.core.flows.CollectSignaturesFlow
+
+import net.corda.core.transactions.SignedTransaction
+
+import java.util.stream.Collectors
+
+import net.corda.core.flows.FlowSession
+
+import net.corda.core.identity.Party
+
+import com.template.contracts.YachtContract
+
+import net.corda.core.transactions.TransactionBuilder
+
+import com.template.states.YachtState
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.contracts.requireThat
+import net.corda.core.identity.AbstractParty
+import java.util.Currency
+import java.util.Date
+
+
+// *********
+// * Flows *
+// *********
+@InitiatingFlow
+@StartableByRPC
+class CreateYachtFlow(private val name: String,
+                      private val type: String,
+                      private val length: Int,
+                      private val beam: Int,
+                      private val builderName: String,
+                      private val yearOfManufacture: Date,
+                      private val grossTonnage: Int,
+                      private val maxSpeed: Int,
+                      private val cruiseSpeed: Int,
+                      private val imageUrls: List<String>,
+                      private val price: Amount<Currency>,
+                      private val forSale: Boolean
+                      ) : FlowLogic<SignedTransaction>() {
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        //Hello World message
+
+        // Step 1. Get a reference to the notary service on our network and our key pair.
+        // Note: ongoing work to support multiple notary identities is still in progress.
+        val notary = serviceHub.networkMapCache.getNotary( CordaX500Name.parse("O=Notary,L=London,C=GB"))
+
+        //Compose the State that carries the Hello World message
+        val output = YachtState(ourIdentity, name, type, length, beam, builderName, yearOfManufacture,grossTonnage, maxSpeed, cruiseSpeed, imageUrls, price, forSale, UniqueIdentifier(),listOf(ourIdentity))
+
+        // Step 3. Create a new TransactionBuilder object.
+        val builder = TransactionBuilder(notary)
+                .addCommand(YachtContract.Commands.Create(), listOf(ourIdentity.owningKey))
+                .addOutputState(output)
+
+        // Step 4. Verify and sign it with our KeyPair.
+        builder.verify(serviceHub)
+        // Sign the transaction
+        val stx = serviceHub.signInitialTransaction(builder)
+
+        // Notarise the transaction and record the state in the ledger
+        return subFlow(FinalityFlow(stx, listOf()))
+    }
+}
---
+++ workflows/src/main/kotlin/com/template/flows/PurchaseYachtDvPFlow.kt
@@ -0,0 +1,84 @@
+package com.template.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.*
+import net.corda.core.identity.*
+import net.corda.core.utilities.ProgressTracker
+
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.finance.workflows.asset.CashUtils
+
+import com.template.states.YachtState
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.node.services.queryBy
+import java.util.Currency
+
+
+// *********
+// * Flows *
+// *********
+@InitiatingFlow
+@StartableByRPC
+class PurchaseYachtDvPFlow(private val newOwner: AbstractParty,
+                           private val yachtId: UniqueIdentifier,
+                           private val price: Amount<Currency>): FlowLogic<SignedTransaction>() {
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call():SignedTransaction {
+        // Query to vault to find the corresponding YachtState - this YachtState will be used as input to the transaction.
+        val yachtStateAndRefs = serviceHub.vaultService.queryBy<YachtState>().states
+        val filteredYachtStateAndRef = yachtStateAndRefs.filter{ it.state.data.linearId == this.yachtId }[0]
+        val yachtState = filteredYachtStateAndRef.state.data
+
+        // Use the withNewOwner() of the Ownable state, get the command and the output state which will be used in the transaction
+        val commandAndState = yachtState.withNewOwner(newOwner)
+
+        // Create the transaction builder
+
+        // Obtain a reference from a notary
+        val notary = serviceHub.networkMapCache.getNotary(CordaX500Name.parse("0=Notary, L=London, C=GB"))
+
+        val txBuilderWithNotary = TransactionBuilder(notary)
+
+        // Generate Spend for the Cash
+         val txAndKeysPair = CashUtils.generateSpend(
+             serviceHub, txBuilderWithNotary,
+             price, ourIdentityAndCert,
+             yachtState.owner!!, emptySet()
+         )
+
+        val txBuilder = txAndKeysPair.first
+
+        txBuilder.addInputState(filteredYachtStateAndRef)
+                 .addOutputState(commandAndState.ownableState)
+                 .addCommand(commandAndState.command, listOf(yachtState.owner.owningKey, newOwner.owningKey))
+
+        txBuilder.verify(serviceHub)
+
+        // Sign the transaction with new keyPair generated for Cash and the node's owningKey
+        val keysToSign = txAndKeysPair.second.plus(ourIdentity.owningKey)
+        val stx = serviceHub.signInitialTransaction(txBuilder, keysToSign)
+
+        // Collect counterparty signature
+        val buyerFlow = initiateFlow(yachtState.owner!!)
+        val ftx = subFlow(CollectSignaturesFlow(stx, listOf(buyerFlow)))
+        return subFlow(FinalityFlow(ftx, (buyerFlow)))
+    }
+}
+
+@InitiatedBy(PurchaseYachtDvPFlow::class)
+class PurchaseYachtDvPFlowResponder(val counterpartySession: FlowSession): FlowLogic<SignedTransaction>(){
+    @Suspendable
+    override fun call():SignedTransaction {
+        subFlow(object : SignTransactionFlow(counterpartySession) {
+            @Throws(FlowException::class)
+            override fun checkTransaction(stx: SignedTransaction) {
+            }
+        })
+        return subFlow(ReceiveFinalityFlow(counterpartySession))
+    }
+}
