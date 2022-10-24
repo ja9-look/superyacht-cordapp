ac7950036c6b6584d52069dc6ee985a8c58926e6
ja9-look <janineluk95@gmail.com>
2022-10-24 14:00:16 +0100

    Create IssueFiatCurrencyFlow

---
+++ workflows/src/main/kotlin/com/template/flows/IssueFiatCurrencyFlow.kt
@@ -0,0 +1,41 @@
+package com.template.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import com.r3.corda.lib.tokens.contracts.states.FungibleToken
+import com.r3.corda.lib.tokens.contracts.utilities.issuedBy
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.identity.Party
+import net.corda.core.utilities.ProgressTracker
+import com.r3.corda.lib.tokens.money.FiatCurrency
+import com.r3.corda.lib.tokens.workflows.flows.rpc.IssueTokens
+import net.corda.core.contracts.Amount
+
+
+// *********
+// * Flows *
+// *********
+
+@StartableByRPC
+class IssueFiatCurrencyFlow(
+    val currency: String,
+    val amount: Long,
+    val recipient: Party): FlowLogic<String>(){
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call(): String {
+        /* Create an instance of the fiat currency token */
+        val token = FiatCurrency.Companion.getInstance(currency)
+
+        /* Create an instance of IssuedTokenType for the fiat currency */
+        val issuedTokenType = token issuedBy ourIdentity
+
+        /* Create an instance of FungibleToken for the fiat currency to be issued */
+        val fungibleToken = FungibleToken(Amount(amount,issuedTokenType),recipient)
+
+        val stx = subFlow(IssueTokens(listOf(fungibleToken), listOf(recipient)))
+        return "Issued $amount $currency token(s) to ${recipient.name.organisation}"
+    }
+    }
+}
